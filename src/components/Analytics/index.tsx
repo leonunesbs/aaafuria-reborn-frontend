1bb195af57148238ccc5cda80e78762b041bcb30
Leonardo Nunes <leonunesbs@gmail.com>
2021-12-14 11:24:23 -0300

    feat: gtag

---
+++ src/components/Analytics/index.tsx
@@ -0,0 +1,28 @@
+import Script from 'next/script';
+import { GA_TRACKING_ID } from 'lib/gtag';
+
+export const Analytics = () => {
+  return (
+    <>
+      <Script
+        id="analytics"
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtm-script"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+    </>
+  );
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,32 @@
 import client from '../services/apollo-client';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { ApolloProvider } from '@apollo/client';
 import { AppProps } from 'next/app';
 import { AuthProvider } from '@/contexts/AuthContext';
 import { ChakraProvider } from '@chakra-ui/react';
+import { useRouter } from 'next/router';
+import * as gtag from 'lib/gtag';
+import { Analytics } from '@/components/Analytics';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: URL) => {
+      gtag.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <ChakraProvider>
       <ApolloProvider client={client}>
         <AuthProvider>
           <Component {...pageProps} />
+          <Analytics />
         </AuthProvider>
       </ApolloProvider>
     </ChakraProvider>
