978f25bcfa4aa5d63bc3fafb625e1fea14b774b8
Leonardo Nunes <leonunesbs@gmail.com>
2021-12-27 17:41:37 -0300

    feat: add eventos button

--- src/components/molecules/HomeMenu/index.tsx
+++ src/components/molecules/HomeMenu/index.tsx
@@ -1,22 +1,36 @@
 import React from 'react';
 import { CustomButtom, CustomChakraNextLink } from '@/components/atoms';
+import { GiPartyPopper } from 'react-icons/gi';
 import { MdStore } from 'react-icons/md';
-
+import { Stack } from '@chakra-ui/react';
 interface HomeMenuProps {
   setLoading: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 export const HomeMenu = ({ setLoading }: HomeMenuProps) => {
   return (
-    <CustomChakraNextLink href="/loja">
-      <CustomButtom
-        leftIcon={<MdStore size="20px" />}
-        onClick={() => {
-          setLoading(true);
-        }}
-      >
-        Loja
-      </CustomButtom>
-    </CustomChakraNextLink>
+    <Stack>
+      <CustomChakraNextLink href="/loja">
+        <CustomButtom
+          leftIcon={<MdStore size="20px" />}
+          onClick={() => {
+            setLoading(true);
+          }}
+        >
+          Loja
+        </CustomButtom>
+      </CustomChakraNextLink>
+      <CustomChakraNextLink href="/eventos">
+        <CustomButtom
+          leftIcon={<GiPartyPopper size="20px" />}
+          onClick={() => {
+            setLoading(true);
+          }}
+          isDisabled
+        >
+          Eventos
+        </CustomButtom>
+      </CustomChakraNextLink>
+    </Stack>
   );
 };
---
+++ src/pages/eventos.tsx
@@ -0,0 +1,14 @@
+import { Layout } from '@/components/templates';
+import { Box } from '@chakra-ui/react';
+
+function Eventos() {
+  return (
+    <Layout title="Loja">
+      <Box maxW="5xl" mx="auto">
+        Eventos
+      </Box>
+    </Layout>
+  );
+}
+
+export default Eventos;
