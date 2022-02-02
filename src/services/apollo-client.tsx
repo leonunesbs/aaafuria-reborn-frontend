4e1229adf113bd2b8819953997b3bbe3f6396eed
Leonardo Nunes <leonunesbs@gmail.com>
2022-02-01 22:12:16 -0300

    fix: fix url

--- src/services/apollo-client.tsx
+++ src/services/apollo-client.tsx
@@ -18,7 +18,7 @@ const defaultOptions: DefaultOptions = {
 
 const httpLink = createHttpLink({
   uri:
-    process.env.NODE_ENV != 'development'
+    process.env.NODE_ENV == 'development'
       ? 'http://192.168.1.110:8000/graphql'
       : 'https://aaafuria-reborn.herokuapp.com/graphql',
   credentials: 'same-origin',
