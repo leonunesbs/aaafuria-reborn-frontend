758b2ebefe092d77d1245553a0482493b4ecb2af
Leonardo Nunes <leonunesbs@gmail.com>
2022-03-10 15:59:16 -0300

    style: them

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,10 +1,11 @@
-import { ThemeConfig, ThemeOverride, extendTheme } from '@chakra-ui/react';
+import { ThemeConfig, extendTheme } from '@chakra-ui/react';
 
 const config: ThemeConfig = {
   initialColorMode: 'light',
 };
 
-const themeOverride: ThemeOverride = {
+export const theme = extendTheme({
+  config,
   fonts: {
     heading: 'AACHENN, sans-serif',
     body: 'Lato, sans-serif',
@@ -22,9 +23,4 @@ const themeOverride: ThemeOverride = {
       },
     },
   },
-};
-
-export const theme = extendTheme({
-  config,
-  themeOverride,
 });
