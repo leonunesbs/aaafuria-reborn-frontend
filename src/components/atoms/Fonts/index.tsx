049951c6e7dd13d15f9b3f807a12e9215ec8528f
Leonardo Nunes <leonunesbs@gmail.com>
2022-04-03 10:38:38 -0300

    fix: fix fonts

--- src/components/atoms/Fonts/index.tsx
+++ src/components/atoms/Fonts/index.tsx
@@ -14,7 +14,7 @@ export const Fonts = () => {
         src: local('AACHENN'),
         url('/fonts/woff2/AACHENN.woff2') format('woff2'), /* will be preloaded */
         url('/fonts/woff/AACHENN.woff') format('woff'),
-        url('/fonts/AACHENN.TTF') format('truetype'),
+        url('/fonts/AACHENN.TTF') format('truetype');
         unicode-range: U+000-5FF;
       }
       /* body */
@@ -27,7 +27,7 @@ export const Fonts = () => {
         src: local('Lato Font'),
         url('/fonts/woff2/Lato-Light.woff2') format('woff2'), /* will be preloaded */
         url('/fonts/woff/Lato-Light.woff') format('woff'),
-        url('/fonts/Lato-Light.ttf') format('truetype'),
+        url('/fonts/Lato-Light.ttf') format('truetype');
         unicode-range: U+000-5FF;
       }
       @font-face {
@@ -38,7 +38,7 @@ export const Fonts = () => {
         src: local('Lato Font'),
         url('/fonts/woff2/Lato-Regular.woff2') format('woff2'), /* will be preloaded */
         url('/fonts/woff/Lato-Regular.woff') format('woff'),
-        url('/fonts/Lato-Regular.ttf') format('truetype'),
+        url('/fonts/Lato-Regular.ttf') format('truetype');
         unicode-range: U+000-5FF;
       }
       @font-face {
@@ -49,7 +49,7 @@ export const Fonts = () => {
         src: local('Lato Font'),
         url('/fonts/woff2/Lato-Bold.woff2') format('woff2'), /* will be preloaded */
         url('/fonts/woff/Lato-Bold.woff') format('woff'),
-        url('/fonts/Lato-Bold.ttf') format('truetype'),
+        url('/fonts/Lato-Bold.ttf') format('truetype');
         unicode-range: U+000-5FF;
       }
       @font-face {
@@ -60,7 +60,7 @@ export const Fonts = () => {
         src: local('Lato Font'),
         url('/fonts/woff2/Lato-Black.woff2') format('woff2'), /* will be preloaded */
         url('/fonts/woff/Lato-Black.woff') format('woff'),
-        url('/fonts/Lato-Black.ttf') format('truetype'),
+        url('/fonts/Lato-Black.ttf') format('truetype');
         unicode-range: U+000-5FF;
       }
       `}
