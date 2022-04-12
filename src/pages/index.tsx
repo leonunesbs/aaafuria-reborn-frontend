9f84fca68d47df19158905b3b8cdc3a45d277e45
Leonardo Nunes <leonunesbs@gmail.com>
2022-04-12 16:15:15 -0300

    refactor: iSR

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -35,7 +35,7 @@ type FeaturePostData = {
   buttonTarget: string;
 };
 
-function Home({ post }: { post: FeaturePostData }) {
+function Home({ post }: { post?: FeaturePostData }) {
   const sejaSocioDiv = useRef<HTMLDivElement>(null);
   const featuresDiv = useRef<HTMLDivElement>(null);
   const { bg, green } = useContext(ColorContext);
@@ -289,68 +289,71 @@ function Home({ post }: { post: FeaturePostData }) {
           </CustomButton>
         </Box>
       </Box>
-      <Box bgColor={bg} py={12} px={{ base: '4', lg: '8' }}>
-        <HStack w={'full'} justify="space-around">
-          <Circle size="15px" bgColor={green} />
-          <Circle size="15px" bgColor={green} />
-          <Circle size="15px" bgColor={green} />
-          <Circle size="15px" bgColor={green} />
-        </HStack>
-        <Stack spacing={8} maxW="7xl" w="full" mx="auto" my={12}>
-          <Stack
-            w="full"
-            justify="space-around"
-            direction={['column', 'column', 'row']}
-          >
-            <Center>
-              <Box
-                width={['400px', '500px', '550px']}
-                height={['400px', '500px', '550px']}
-                position="relative"
-              >
-                <ChakraNextImage
-                  placeholder="blur"
-                  blurDataURL={post.image}
-                  layout="fill"
-                  objectFit="cover"
-                  src={post.image}
-                  quality={50}
-                  alt="logo"
-                  draggable={false}
-                  rounded="md"
-                />
-              </Box>
-            </Center>
-            <Stack justify={'space-between'}>
-              <Stack justify={'center'} h="100%" my={4}>
-                <Heading as="h2" textAlign={['left', 'left', 'right']}>
-                  {post.title}
-                </Heading>
-                <Text as="h4" maxW="md" textAlign={['left', 'left', 'right']}>
-                  {post.content}
-                </Text>
-              </Stack>
-              <Stack>
-                <CustomButton
-                  variant={'solid'}
-                  onClick={() => sejaSocioDiv.current?.scrollIntoView()}
+      {post && (
+        <Box bgColor={bg} py={12} px={{ base: '4', lg: '8' }}>
+          <HStack w={'full'} justify="space-around">
+            <Circle size="15px" bgColor={green} />
+            <Circle size="15px" bgColor={green} />
+            <Circle size="15px" bgColor={green} />
+            <Circle size="15px" bgColor={green} />
+          </HStack>
+          <Stack spacing={8} maxW="7xl" w="full" mx="auto" my={12}>
+            <Stack
+              w="full"
+              justify="space-around"
+              direction={['column', 'column', 'row']}
+            >
+              <Center>
+                <Box
+                  width={['400px', '500px', '550px']}
+                  height={['400px', '500px', '550px']}
+                  position="relative"
                 >
-                  Seja sócio
-                </CustomButton>
-                <CustomChakraNextLink href={post.buttonTarget}>
-                  <CustomButton variant={'outline'}>Saiba mais</CustomButton>
-                </CustomChakraNextLink>
+                  <ChakraNextImage
+                    placeholder="blur"
+                    blurDataURL={post.image}
+                    layout="fill"
+                    objectFit="cover"
+                    src={post.image}
+                    quality={50}
+                    alt="logo"
+                    draggable={false}
+                    rounded="md"
+                  />
+                </Box>
+              </Center>
+              <Stack justify={'space-between'}>
+                <Stack justify={'center'} h="100%" my={4}>
+                  <Heading as="h2" textAlign={['left', 'left', 'right']}>
+                    {post.title}
+                  </Heading>
+                  <Text as="h4" maxW="md" textAlign={['left', 'left', 'right']}>
+                    {post.content}
+                  </Text>
+                </Stack>
+                <Stack>
+                  <CustomButton
+                    variant={'solid'}
+                    onClick={() => sejaSocioDiv.current?.scrollIntoView()}
+                  >
+                    Seja sócio
+                  </CustomButton>
+                  <CustomChakraNextLink href={post.buttonTarget}>
+                    <CustomButton variant={'outline'}>Saiba mais</CustomButton>
+                  </CustomChakraNextLink>
+                </Stack>
               </Stack>
             </Stack>
           </Stack>
-        </Stack>
-        <HStack w={'full'} justify="space-around" mb={10}>
-          <Circle size="15px" bgColor={green} />
-          <Circle size="15px" bgColor={green} />
-          <Circle size="15px" bgColor={green} />
-          <Circle size="15px" bgColor={green} />
-        </HStack>
-      </Box>
+          <HStack w={'full'} justify="space-around" mb={10}>
+            <Circle size="15px" bgColor={green} />
+            <Circle size="15px" bgColor={green} />
+            <Circle size="15px" bgColor={green} />
+            <Circle size="15px" bgColor={green} />
+          </HStack>
+        </Box>
+      )}
+
       <Box ref={sejaSocioDiv} bgColor={green} id="seja-socio" py={12} px={2}>
         <PageHeading as="h2" textColor={bg}>
           Junte-se a nós, seja um <Text as="span">sócio Fúria</Text>!
@@ -382,6 +385,7 @@ export async function getStaticProps() {
     props: {
       post: data.featurePost,
     },
+    revalidate: 60 * 60 * 1, // Every 1 hour
   };
 }
 
