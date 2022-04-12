ed141749d8e0e0de072d0b4e90dbf34ca8dc7e02
Leonardo Nunes <leonunesbs@gmail.com>
2022-04-12 11:20:18 -0300

    refactor: downgrade react

--- src/components/molecules/Header/index.tsx
+++ src/components/molecules/Header/index.tsx
@@ -2,6 +2,13 @@ import {
   Avatar,
   Box,
   Center,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
   Flex,
   HStack,
   Menu,
@@ -10,19 +17,27 @@ import {
   MenuGroup,
   MenuItem,
   MenuList,
+  Stack,
   chakra,
+  useDisclosure,
 } from '@chakra-ui/react';
 import {
   ColorModeToggle,
   CustomButton,
   CustomChakraNextLink,
+  CustomIconButton,
 } from '@/components/atoms';
+import { FaDrum, FaVolleyballBall } from 'react-icons/fa';
+import { MdManageAccounts, MdPerson, MdStore } from 'react-icons/md';
 import { gql, useQuery } from '@apollo/client';
+import { useContext, useRef } from 'react';
 
+import { AiFillHome } from 'react-icons/ai';
 import { AuthContext } from '@/contexts/AuthContext';
 import { ColorContext } from '@/contexts/ColorContext';
+import { GiPartyPopper } from 'react-icons/gi';
+import Hamburger from 'hamburger-react';
 import NextImage from 'next/image';
-import { useContext } from 'react';
 import { useRouter } from 'next/router';
 
 const GET_SOCIO = gql`
@@ -43,8 +58,10 @@ const GET_SOCIO = gql`
 
 export const Header = () => {
   const router = useRouter();
+  const btnRef = useRef<HTMLButtonElement>(null);
+  const { isOpen, onToggle, onClose } = useDisclosure();
   const { bg, green } = useContext(ColorContext);
-  const { isAuthenticated, token, signOut } = useContext(AuthContext);
+  const { isAuthenticated, token, signOut, isStaff } = useContext(AuthContext);
   const { data } = useQuery(GET_SOCIO, {
     context: {
       headers: {
@@ -134,17 +151,6 @@ export const Header = () => {
                       : '2px solid gray'
                   }
                 />
-                <Avatar
-                  size="md"
-                  display={['flex', 'flex', 'none']}
-                  name={data?.socioAutenticado?.nome}
-                  src={data?.socioAutenticado?.avatar}
-                  border={
-                    data?.socioAutenticado?.isSocio
-                      ? '2px solid green'
-                      : '2px solid gray'
-                  }
-                />
               </MenuButton>
               <MenuList>
                 <MenuGroup title="Menu">
@@ -188,9 +194,130 @@ export const Header = () => {
               <CustomButton>Entrar</CustomButton>
             </CustomChakraNextLink>
           )}
+          <CustomIconButton
+            ref={btnRef}
+            aria-label="hamburguer-menu"
+            onClick={onToggle}
+            icon={<Hamburger toggled={isOpen} size={24} />}
+          />
         </HStack>
       </Flex>
       <Flex flexGrow={1} bgColor={green} h={1} />
+      <Drawer isOpen={isOpen} placement="left" onClose={onClose} size="xs">
+        <DrawerOverlay />
+        <DrawerContent bgColor={green} pr={6}>
+          <DrawerCloseButton color={bg} />
+          <DrawerHeader>
+            <CustomChakraNextLink
+              href="/"
+              chakraLinkProps={{
+                onClick: onClose,
+              }}
+            >
+              <Box
+                height={['80px', '100px']}
+                width={['130px', '160px']}
+                position="relative"
+              >
+                <ChakraNextImage
+                  placeholder="blur"
+                  layout="fill"
+                  objectFit="cover"
+                  src={'/logo-aaafuria-h.webp'}
+                  blurDataURL={'/logo-aaafuria-h.webp'}
+                  quality={1}
+                  alt="logo"
+                  mx="auto"
+                  mb={{ base: '8', md: '12' }}
+                  draggable={false}
+                />
+              </Box>
+            </CustomChakraNextLink>
+          </DrawerHeader>
+
+          <DrawerBody>
+            <Stack mt={10}>
+              <CustomChakraNextLink href={'/'}>
+                <CustomButton
+                  isActive={router.asPath == '/'}
+                  variant={'solid'}
+                  justifyContent={'flex-start'}
+                  leftIcon={<AiFillHome size="20px" />}
+                >
+                  Início
+                </CustomButton>
+              </CustomChakraNextLink>
+              <CustomChakraNextLink href={'/loja'}>
+                <CustomButton
+                  isActive={router.asPath == '/loja'}
+                  variant={'solid'}
+                  justifyContent={'flex-start'}
+                  leftIcon={<MdStore size="20px" />}
+                >
+                  Loja
+                </CustomButton>
+              </CustomChakraNextLink>
+              <CustomChakraNextLink href={'/atividades'}>
+                <CustomButton
+                  isActive={router.asPath == '/atividades'}
+                  variant={'solid'}
+                  py={10}
+                  justifyContent={'flex-start'}
+                  leftIcon={
+                    <Stack>
+                      <FaVolleyballBall size="20px" />
+                      <FaDrum size="20px" />
+                    </Stack>
+                  }
+                >
+                  Atividades
+                </CustomButton>
+              </CustomChakraNextLink>
+              <CustomChakraNextLink href={'/eventos'}>
+                <CustomButton
+                  isActive={router.asPath == '/eventos'}
+                  variant={'solid'}
+                  py={10}
+                  justifyContent={'flex-start'}
+                  leftIcon={<GiPartyPopper size="20px" />}
+                >
+                  Eventos
+                </CustomButton>
+              </CustomChakraNextLink>
+            </Stack>
+          </DrawerBody>
+
+          <DrawerFooter>
+            <Box w="full">
+              <Stack>
+                <CustomChakraNextLink href={'/areasocio'}>
+                  <CustomButton
+                    isActive={router.asPath == '/areasocio'}
+                    variant={'solid'}
+                    justifyContent={'flex-start'}
+                    leftIcon={<MdPerson size="20px" />}
+                  >
+                    Área do Sócio
+                  </CustomButton>
+                </CustomChakraNextLink>
+                {isStaff && (
+                  <CustomChakraNextLink href={'/areadiretor'}>
+                    <CustomButton
+                      isActive={router.asPath == '/areadiretor'}
+                      variant={'solid'}
+                      colorScheme="yellow"
+                      justifyContent={'flex-start'}
+                      leftIcon={<MdManageAccounts size="20px" />}
+                    >
+                      Área do Diretor
+                    </CustomButton>
+                  </CustomChakraNextLink>
+                )}
+              </Stack>
+            </Box>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
     </>
   );
 };
--- src/contexts/ColorContext.tsx
+++ src/contexts/ColorContext.tsx
@@ -4,6 +4,7 @@ import { useColorModeValue } from '@chakra-ui/react';
 interface ColorContextProps {
   green: string;
   bg: string;
+  invertedBg: string;
 }
 
 interface ColorProviderProps {
@@ -13,14 +14,16 @@ interface ColorProviderProps {
 export const ColorContext = createContext({} as ColorContextProps);
 
 export function ColorProvider({ children }: ColorProviderProps) {
-  const green = useColorModeValue('green.600', 'green.200');
+  const green = useColorModeValue('green.600', 'green.300');
   const bg = useColorModeValue('white', 'gray.800');
+  const invertedBg = useColorModeValue('gray.800', 'white');
 
   return (
     <ColorContext.Provider
       value={{
         green,
         bg,
+        invertedBg,
       }}
     >
       {children}
