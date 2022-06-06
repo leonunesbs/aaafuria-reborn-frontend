2ed5ccfc7bc9c4d23d736eebbf9e56705428f2ee
Leonardo Nunes <leonunesbs@gmail.com>
2022-06-06 13:13:14 -0300

    fix: graphlql url

--- src/services/apollo-client.tsx
+++ src/services/apollo-client.tsx
@@ -16,7 +16,7 @@ const defaultOptions: DefaultOptions = {
 const uploadLink = createUploadLink({
   uri:
     process.env.NODE_ENV === 'development'
-      ? 'http://192.168.5.150:8000/graphql'
+      ? 'http://192.168.5.155:8000/graphql'
       : `${process.env.BACKEND_DOMAIN}/graphql`,
   credentials: 'same-origin',
 });
